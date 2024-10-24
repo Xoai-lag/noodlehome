ddb1388eec422bea1acd99374a65db7df12123fa
Xoai-lag <dh52200568@student.stu.edu.vn>
2024-10-25 00:29:54 +0700

    note lai lenh tao file sever

--- noodlehome.go
+++ noodlehome.go
@@ -5,7 +5,14 @@ import (
 )
 
 func main() {
+	// Tạo một file server để phục vụ các file tĩnh từ thư mục "./static"
 	fs := http.FileServer(http.Dir("./static"))
+
+	// Đăng ký file server cho đường dẫn gốc ("/")
+	// Tức là mọi yêu cầu đến http://localhost:8080/ sẽ được xử lý bởi fs
 	http.Handle("/", fs)
+
+	// Khởi động server và lắng nghe trên cổng 8080
+	// Tham số nil sử dụng http.DefaultServeMux cho việc xử lý các yêu cầu
 	http.ListenAndServe(":8080", nil)
 }
